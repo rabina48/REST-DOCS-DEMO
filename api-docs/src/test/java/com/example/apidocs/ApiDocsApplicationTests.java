8c645dc362e2d875001d2f7f2b2bc2881aff994b
rabina48 <45580944+rabina48@users.noreply.github.com>
2021-06-18 07:09:43 +0545

    Add files via upload

---
+++ api-docs/src/test/java/com/example/apidocs/ApiDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
